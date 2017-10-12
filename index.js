18af5a9d6c734b3b2586c4ee088378489acfa77d
Derek Soucie <dsoucie@indiana.edu>
2017-10-12 16:12:52 -0700

    works

--- index.js
+++ index.js
@@ -1,50 +1,72 @@
 const express = require('express');
 const axios = require('axios');
 const mongodb = require('mongodb').MongoClient;
+const dbURL = 'mongodb://dsoucie:devpassword@ds117605.mlab.com:17605/image-search-abstraction-layer';
 
 var app = express();
 
-app.get('/:query', (request, response) => {
-  const query = request.params.query;
-  const offset = request.query.offset;
-  console.log('offset: ', offset);
-  // const url = `https://www.google.com/search?tbm=isch&source=hp&biw=950&bih=970&q=trump&oq=${query}&gs_l=img.3..0l10.5097.5581.0.5911.5.5.0.0.0.0.128.332.0j3.3.0....0...1.1.64.img..2.3.330....0.PQyEj0jvBCA`;
-  
-  // axios.get(url)
-  //   .then( (googleResponse) => {
-  //     response.json(googleResponse.data);
-  //   });
+app.set('view engine', 'pug');
 
-  response.send(`the offset is ${offset}`);
+app.get('/search/:query', (request, response) => {
+  const query = request.params.query;
+  const offset = request.query.offset || 0;
+  const start = String(Number(offset) + 1);
+
+  console.log('query: ', query);
+  console.log('offset: ', offset);
+  console.log('start: ', start);
+
+  mongodb.connect(dbURL, (error, db) => {
+    var myCol = db.collection('myCol');
+
+    myCol.insert({
+      searchQuery: query,
+      unixTimestamp: Math.round(Date.now() / 1000),
+    });
+
+  });
+
+  const url = `https://www.googleapis.com/customsearch/v1?key=AIzaSyDKcOIgYFECBl3wZ_iHtyw7CODoNuXopNQ&cx=010287229141200450979:t6euai2tuhe&q=${query}&start=${start}&searchType=image`;
+  console.log('url: ', url);
+
+  axios.get(url)
+    .then( (googleResponse) => {
+      const pictureArray = googleResponse.data.items;
+      var responseArray = [];
+
+      pictureArray.forEach( (picture) => {
+        let pictureObj = {};
+        pictureObj.pageURL = picture.image.contextLink;
+        pictureObj.imageURL = picture.link;
+        pictureObj.altText = picture.snippet;
+        responseArray.push(pictureObj);
+      })
+
+      response.json(responseArray);
+    });
 
 });
-  
-  app.get('*', (request, response) => {
-    response.send('please enter search query.');
-  })
-  
-  app.listen(process.env.PORT || 8080);
-  
-  //   axios.request({
-  //     method: 'get',
-  //     url: url,
-  //     headers: { "Authorization": "Client-ID 750ce936a288371" }
-  //   })
-  //     .then( (imgurResponse) => {
-  //       console.log('received')
-  //       var galleries = imgurResponse.data.data;
-  //       var results = [];
-  //       galleries.forEach( (gallery) => {
-  //         try {
-  //           gallery.images.forEach( (image) => {
-  //             results.push(image.link);
-  //           });
-  //         } catch (e) {
-  //           console.log(e);
-  //         }
-  //       });
-  //       response.json(galleries);
-  //     });
-  // })
 
-  //AIzaSyAWGP9yqBcC2P9sJnqj9aoJCzqPfLQ0gs4
+app.get('/history', (request, response) => {
+  mongodb.connect(dbURL, (error, db) => {
+    var myCol = db.collection('myCol');
+
+    myCol.find({}).toArray( (error, documents) => {
+      var allHistory = documents;
+      
+      allHistory.sort((a, b) => {
+        return b.unixTimestamp - a.unixTimestamp;
+      });
+  
+      var recent = allHistory.slice(0, 9);
+  
+      response.json(recent);
+    });  
+  });
+});
+  
+app.get('*', (request, response) => {
+  response.render('index');
+})
+
+app.listen(process.env.PORT || 8080);
